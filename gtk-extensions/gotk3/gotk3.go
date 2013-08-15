5f7cf87b719577bb67230bd9ca0f1e6e7fc7666c
Per Loenn Wege <per.loenn.wege@gmail.com>
2013-08-15 08:07:06 +0200

    app_indicator_secondary_activate_target

--- gtk-extensions/gotk3/gotk3.go
+++ gtk-extensions/gotk3/gotk3.go
@@ -3,7 +3,6 @@ package gotk3
 //#cgo CFLAGS: -I/usr/include/libappindicator3-0.1
 //#cgo LDFLAGS: -lappindicator3
 //#cgo pkg-config: gtk+-3.0
-//#include <gtk/gtk.h>
 //#include <libappindicator/app-indicator.h>
 import "C"
 import "unsafe"
@@ -26,6 +25,6 @@ func (indicator *AppIndicatorGotk3) SetMenu(menu *gtk.Menu) {
 	C.app_indicator_set_menu((*C.AppIndicator)(indicator.IndicatorPtr), (*C.GtkMenu)(unsafe.Pointer(menu.Native())))
 }
 
-/*func (indicator *AppIndicator) SetSecondaryActivateTarget(menuItem *gtk.Widget) {
+func (indicator *AppIndicatorGotk3) SetSecondaryActivateTarget(menuItem *gtk.Widget) {
 	C.app_indicator_set_secondary_activate_target((*C.AppIndicator)(indicator.IndicatorPtr), (*C.GtkWidget)(unsafe.Pointer(menuItem.Native())))
-}*/
+}
